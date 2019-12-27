cbc8543d32375db7e45d5492ebffe3047996e07a
chrisdavidashcroft@gmail.com <chrisdavidashcroft@gmail.com>
2019-12-27 18:43:09 +0000

    Adjusted values on SEAD. Updated Mission to v0.4.16.

--- Scripts/LivesSystem.lua
+++ Scripts/LivesSystem.lua
@@ -1,13 +1,16 @@
--- Operation Enduring Odyssey Lives System Initial Draft --
+-- Operation Enduring Odyssey Lives System --
 
 -- Define Required Variables and Functions --
 LivesRemaining = 300
 LivesAirborne = 0
 LivesLost = 0
+LivesMIA = 0
+LivesAWOL = 0
 Takeoff = {}
 Landing = {}
 Death = {}
 Eject = {}
+Disconnect = {}
 --Revived = {}
 MissionFailure = {}
 
@@ -20,11 +23,19 @@ MissionFailure = {}
 function PlayerTakeoff()
 	LivesRemaining = LivesRemaining - 1
 	LivesAirborne = LivesAirborne + 1
+		if LivesRemaining < 0
+			then
+				LivesRemaining = 0
+		end
 end
 
 function PlayerLand()
 	LivesRemaining = LivesRemaining + 1
 	LivesAirborne = LivesAirborne - 1
+		if LivesAirborne < 0
+			then
+				LivesAirborne = 0
+		end
 end
 
 function PlayerDie()
@@ -36,37 +47,38 @@ function PlayerDie()
 	end
 end
 
+function PlayerMIA()
+	LivesMIA = LivesMIA + 1
+	LivesAirborne = LivesAirborne - 1
+		if LivesAirborne < 0
+			then
+				LivesAirborne = 0
+		end
+end
+
 function PlayerRecovered()
 	LivesLost = LivesLost - 1
 	LivesRemaining = LivesRemaining + 1
 end
 
--- Define Simple Functions for Amending Arithmetic Values of Variables by 1 --
-
-function LivesRemainingUp()
-	LivesRemaining = LivesRemaining - 1
-end
-
-function LivesRemainingDown()
-	LivesRemaining = LivesRemaining + 1
-end
-
-function LivesAirborneUP()
-	LivesAirborne = LivesAirborne + 1
-end
-
-function LivesAirborneDown()
+function PlayerAWOL()
 	LivesAirborne = LivesAirborne - 1
+	LivesAWOL = LivesAWOL + 1
+		if LivesAirborne < 0
+			then
+				LivesAirborne = 0
+		end
 end
 
-function LivesLostUp()
-	LivesLost = LivesLost + 1
+function PlayerNotAWOL()
+	LivesAWOL = Lives AWOL - 1
+	LivesAirborne = Lives Airborne + 1
+		if LivesAWOL < 0
+			then
+				LivesAWOL = 0
+		end
 end
-
-function LivesLostDown()
-	LivesLost = LivesLost - 1
-end
-
+	
 -- F10 Menu Check Remaning Lives --
 
 function PrintLives()
@@ -82,100 +94,79 @@ F10CheckLives()
 -- Define Actions on Player Events --
 
 function Takeoff:onEvent(event)
-	if event.id == 3 and event.initiator ~= nil
+	if (event.id == 3 and event.place:getTypeName() == "Al Minhad AB" and event.initiator:getPlayerName() ~= nil) or (event.id == 3 and event.place:getTypeName() == "Al Dhafra AB" and event.initiator:getPlayerName() ~= nil) or (event.id == 3 and event.place:getTypeName() == "Stennis - airbase" and event.initiator:getPlayerName() ~= nil) or (event.id == 3 and event.place:getTypeName() == "LHA_Tarawa - airbase" and event.initiator:getPlayerName() ~= nil)
 		then
 			PlayerTakeoff()
 			local DepartedPilot = event.initiator
 			trigger.action.outTextForGroup(DepartedPilot:getGroup():getID(), "You have taken off and a life has been removed from the team. Land safely at either Al Dhafra, Al Minhad or the Carrier to return your life to the pool. Good luck!", 10, 1)
-			env.info("TAKEOFF EVENT RUN")
+			--env.info("TAKEOFF EVENT RUN")
+	elseif event.id == 3 and event.initiator:getPlayerName() ~= nil
+		then
+			PlayerNotAWOL()
+			local NotAWOLPilot = event.initiator
+			trigger.action.outTextForGroup(NotAWOLPilot:getGroup():getID(), "You have taken off again and are no longer considered AWOL. Return to Al Dhafra, Al Minhad or the Carrier to return your life to the pool.", 30, 1)
 		end
 end
 
---[[function Landing:onEvent(event) -- Check Surface Version
-		if event.id == 4 and event.initiator ~= nil
+function Landing:onEvent(event)
+	if (event.id == 4 and event.place:getTypeName() == "Al Minhad AB" and event.initiator:getPlayerName() ~= nil) or (event.id == 4 and event.place:getTypeName() == "Al Dhafra AB" and event.initiator:getPlayerName() ~= nil) or (event.id == 4 and event.place:getTypeName() == "Stennis - airbase" and event.initiator:getPlayerName() ~= nil) or (event.id == 3 and event.place:getTypeName() == "LHA_Tarawa - airbase" and event.initiator:getPlayerName() ~= nil)
 		then 
 			--env.info("LANDING DETECTED")
 			local LandedPilot = event.initiator
-			--env.info("INITIATOR SET")
-			local LandingPoint = LandedPilot:getPoint()
-			--env.info("GOT LANDING COORDINATES")
-			local LandingPointSurface = land.getSurfaceType(LandingPoint)
-			--env.info("GOT LANDING SURFACE TYPE")
-			env.info("LANDING SURFACE = "  .. LandingPointSurface)
-				if LandingPointSurface == 5
-					then
-						PlayerLand()
-						local LandedPilot = event.initiator
-						trigger.action.outTextForGroup(LandedPilot:getGroup(), "Welcome back! You have landed safely and your life has been returned to the team. Great job!"  10, 1)
-						env.info("LANDING EVENT RUN")
-				end
-		end
-end]]--
-
-function Landing:onEvent(event) -- Timer Check Version
-		if event.id == 4 and event.initiator ~= nil
-		then 
-			env.info("LANDING DETECTED")
+			timer.scheduleFunction(LandingLifeCheck, LandedPilot, timer.getTime()+1)
+			--env.info("SCHEDULED CHECK FUNCTION")
+	elseif event.id == 4 and event.initiator:getPlayerName() ~= nil
+		then
+			--env.info("LANDING AT WRONG AIRBASE DETECTED")
 			local LandedPilot = event.initiator
-			LandedPilotCategory = LandedPilot:getCategory()
-			env.info(LandedPilotCategory)
-			timer.scheduleFunction(LandingLifeCheck, {LandedPilot}, timer.getTime()+3)
-			env.info("SCHEDULED CHECK FUNCTION")
-			end
-end
-
-
-function LandingLifeCheck(LandedPilot)
-	env.info("RUNNING LIFE CHECK")
-	local PilotExists = LandedPilot.isExist()
-		if
-			PilotExists == nil
-				then
-					env.info("PILOT EXISTS IS NIL")
-		else if 
-			PilotExists == true
-				then
-					env.info("PILOT EXISTS")
-		else if
-			PilotsExists == false
-				then
-					env.info("PILOT IS DEAD")
-			end
-		end
+			timer.scheduleFunction(AWOLLandingLifeCheck, LandedPilot, timer.getTime()+1)
+			--env.info("SCHEDULED AWOL CHECK FUNCTION")
 	end
 end
-	--[[local PilotsLife = Unit.getLife(PilotOnGround)
-	--env.info(PilotsLife)
-		--if PilotsLife == nil
-			--then
-				--env.info("LIFE IS NIL")
-		else if PilotsLife > 0
-			then
-				env.info("PLAYER IS ALIVE")
-				PlayerLand()
-				trigger.action.outTextForGroup(PilotOnGround:getGroup(), "Welcome back! You have landed safely and your life has been returned to the team. Great job!", 10, 1)
-				env.info("LANDING EVENT RUN")
-			end
-		end
-end--]]
 
+function LandingLifeCheck(PilotOnGround)
+	--env.info("RUNNING LIFE CHECK")
+	local PilotsLife = PilotOnGround:getLife()
+	--env.info(PilotsLife)
+		if PilotsLife > 0
+			then
+				--env.info("PLAYER IS ALIVE")
+				PlayerLand()
+				trigger.action.outTextForGroup(PilotOnGround:getGroup():getID(), "Welcome back! You have landed safely and your life has been returned to the team. Great job!", 10, 1)
+				--env.info("LANDING EVENT RUN")
+			end
+end
+		
+function AWOLLandingLifeCheck(PilotOnGround)
+	--env.info("RUNNING AWOL LIFE CHECK")
+	local PilotsLife = PilotOnGround:getLife()
+	--env.info(PilotsLife)
+		if PilotsLife > 0
+			then
+				--env.info("PLAYER IS AWOL")
+				PlayerAWOL()
+				trigger.action.outTextForGroup(PilotOnGround:getGroup():getID(), "You have successfully landed, but at the wrong airbase! You are currently considered AWOL. Return to either Al Dhafra, Al Minhad or the Carrier to return your life to the pool.", 30, 1)
+				--env.info("AWOL EVENT RUN")
+			end
+end
+		
 function Death:onEvent(event)
-	if event.id == 9 and event.initiator ~= nil
+	if event.id == 9 and event.initiator:getPlayerName() ~= nil
 		then
 			PlayerDie()
 			local KilledPilot = event.initiator
 			trigger.action.outTextForGroup(KilledPilot:getGroup():getID(), "You have been killed in action. Your life has been added to the death toll. Be more careful next time!", 10, 1)
-			env.info("DEATH EVENT RUN")
+			--env.info("DEATH EVENT RUN")
 		end
 end
 
 function Eject:onEvent(event)
-	if event.id == 6 and event.initiator ~= nil
+	if event.id == 6 and event.initiator:getPlayerName() ~= nil
 		then
-			PlayerDie()
+			PlayerMIA()
 			local EjectedPilot = event.initiator
 			trigger.action.outTextForCoalition(2, EjectedPilot:getPlayerName().." has ejected. CSAR Available.", 10, 1) -- This line will be removed as Ali's CSAR Script sends the CSAR message.
-			env.info("EJECT EVENT RUN")
+			--env.info("EJECT EVENT RUN")
 		end
 end
 
@@ -184,9 +175,17 @@ end
 		then
 			PlayerRecovered()
 			trigger.action.outTextForGroup(RECOVERYPILOTSNAME, "You have successfully returned a downed pilot to base, and a life has been added to the pool. Great job!", 10, 1)
-			env.info("RECOVERY EVENT RUN")
+			--env.info("RECOVERY EVENT RUN")
 		end
 end]]--
+
+function Disconnect:onEvent(event)
+	if event.id == 20 and event.initiator:inAir() == true
+		then
+			PlayerDie()
+			--env.info("DISCONNECT EVENT RUN")
+		end
+end
 
 -- Define Function for actioning Mission Failure --
 
@@ -195,7 +194,7 @@ function RestartMission()
 end
 
 function MissionFailure:onEvent(event)
-	if event.id == 9 and event.initiator ~= nil and LivesLost > 300
+	if event.id == 9 and event.initiator:getPlayerName() ~= nil and LivesLost > 300
 		then
 			trigger.action.outTextForCoalition(2, "The death toll of Operation Enduring Odyssey has soared too high. Public opinion of the Operation has fallen and units are being pulled out of the Persian Gulf. Mission Failed.", 60, 1)
 			timer.scheduleFunction(RestartMission, {}, timer.getTime()+60)
@@ -208,3 +207,4 @@ world.addEventHandler(Death)
 world.addEventHandler(Eject)
 --world.addEventHandler(Revived)
 world.addEventHandler(MissionFailure)
+world.addEventHandler(Disconnect)
